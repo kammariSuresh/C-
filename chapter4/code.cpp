38567b7ae2f7e088a8a3c09fd24b8518dd111c0c
Suresh <kammarisuresh360@gmail.com>
2026-08-06 21:02:46 +0530

    commit4

--- chapter4/code.cpp
+++ chapter4/code.cpp
@@ -41,14 +41,14 @@ int main() {
     //     cout << endl;
     // }
 // Reverse Triangel pattern eg-2 Reverse loop
-    int n;
-    cin>>n;
-    for (int i=1; i<=n; i++){
-        for (int j=i; j>0; j--){
-            cout << j << " ";
-        }
-        cout << endl;
-    }
+    // int n;
+    // cin>>n;
+    // for (int i=1; i<=n; i++){
+    //     for (int j=i; j>0; j--){
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
 
 // Triangle pattern with Alphabits
     // int n;
@@ -59,6 +59,18 @@ int main() {
     //     }
     //     cout<< endl;
     // }
+
+// Floyd's Triangle Pattern
+    int n;
+    cin>>n;
+    int count = 1;
+    for (int i =0; i<n; i++) {
+        for (int j=1; j<=i+1; j++ ){
+            cout<< count << " ";
+            count = count + 1;
+        }
+        cout << endl;
+    }
 
 }
 
