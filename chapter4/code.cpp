b3fa05ca9b0be3eb9d88b455ba2ec58c5e98544f
Suresh <kammarisuresh360@gmail.com>
2026-08-09 22:25:11 +0530

    commit7

--- chapter4/code.cpp
+++ chapter4/code.cpp
@@ -66,7 +66,7 @@ int main() {
     int count = 1;
     for (int i =0; i<n; i++) {
         for (int j=1; j<=i+1; j++ ){
-            cout<< count << " ";
+            cout<< count << "";
             count = count + 1;
         }
         cout << endl;
