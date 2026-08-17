656d7003c63049b32bad05f3f9b2ee81d48d980e
Suresh <kammarisuresh360@gmail.com>
2026-08-17 22:06:02 +0530

    commit15

--- chapter4/code.cpp
+++ chapter4/code.cpp
@@ -111,18 +111,30 @@ int main() {
     //     cout << endl;
     // }
 // Inverted Triangle pattern with alphabets
+    // int n;
+    // cin >> n;
+    // for (int i=0; i<n; i++) {
+    //     for (int j=0; j<i; j++){
+    //         cout << "  ";
+    //     }
+    //     for (int j=1; j <= n-i; j++){
+    //         cout << char(65+i) << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+// pyramid pattern 
+
     int n;
     cin >> n;
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<i; j++){
-            cout << "  ";
+    for (int i=0; i<n; i++){
+        for (int j=0; j<i; j++) {
+            cout << "*";
         }
-        for (int j=1; j <= n-i; j++){
-            cout << char(65+i) << " ";
-        }
-        cout << endl;
-
     }
+
+
+
     
 
 }
