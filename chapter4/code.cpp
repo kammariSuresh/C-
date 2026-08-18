36b6235a5a42015e9db518061af1e1f7895e8bef
Suresh <kammarisuresh360@gmail.com>
2026-08-18 21:24:24 +0530

    commit17

--- chapter4/code.cpp
+++ chapter4/code.cpp
@@ -129,14 +129,14 @@ int main() {
     cin >> n;
     for (int i=0; i<n; i++){
         for (int j=0; j<n-i-1; j++) {
-            cout << "  ";
+            cout << " ";
         }
         for (int j=0; j<i+1; j++) {
-            cout << j+1 << " ";
+            cout << j+1 << "";
         }
         // cout << endl;
         for (int j=i; j>0; j--) {
-            cout << j << " ";
+            cout << j << "";
         }
         cout << endl;
     }
