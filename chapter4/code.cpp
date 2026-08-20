687a72ca0b8f60860e2d47a903f730a254df41cb
Suresh <kammarisuresh360@gmail.com>
2026-08-20 21:30:03 +0530

    commit19

--- chapter4/code.cpp
+++ chapter4/code.cpp
@@ -125,22 +125,25 @@ int main() {
 
 // pyramid pattern 
 
+    // int n;
+    // cin >> n;
+    // for (int i=0; i<n; i++){
+    //     for (int j=0; j<n-i-1; j++) {
+    //         cout << "  ";
+    //     }
+    //     for (int j=0; j<i+1; j++) {
+    //         cout << j+1 << " ";
+    //     }
+    //     // cout << endl;
+    //     for (int j=i; j>0; j--) {
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
     int n;
     cin >> n;
-    for (int i=0; i<n; i++){
-        for (int j=0; j<n-i-1; j++) {
-            cout << "  ";
-        }
-        for (int j=0; j<i+1; j++) {
-            cout << j+1 << " ";
-        }
-        // cout << endl;
-        for (int j=i; j>0; j--) {
-            cout << j << " ";
-        }
-        cout << endl;
-    }
-
+    cout << n;
 
 
     
