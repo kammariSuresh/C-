6b95cac214e58ef7e2271931beb90c9762232a11
Suresh <kammarisuresh360@gmail.com>
2026-08-25 21:00:34 +0530

    commit28

--- chapter3/code.cpp
+++ chapter3/code.cpp
@@ -2,5 +2,38 @@
 using namespace std;
 
 int main() {
-    cout <<"kammari suresh";
+    // cout <<"kammari suresh";
+    // Butterlfly patterns
+int n;
+    cin >> n;
+    for (int i=0; i<n; i++) {
+        // stars1.1
+        for (int j=0; j<i+1; j++) {
+            cout << "* ";
+        }
+        // spaces 
+        for (int j=0; j< 2*(n-i-1); j++) {
+            cout << "  ";
+        } 
+        // stars1.2
+        for (int j=0; j<i+1; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for (int i=0; i<n-1; i++) {
+        // stars2.1 
+        for (int j=0; j<(n-1)-i; j++) {
+            cout << "* ";
+        } 
+        // spaces
+        for (int j=0; j<2*(i+1); j++){
+            cout << "  ";
+        }
+        // stars2.2
+        for (int j=0; j<(n-1)-i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
 }
