84f4c756683e00ebfd3c4b01d30684902b880422
Suresh <kammarisuresh360@gmail.com>
2026-08-25 20:38:38 +0530

    commit25

--- chapter4/code.cpp
+++ chapter4/code.cpp
@@ -144,44 +144,81 @@ int main() {
 // Diamonad pattern 
 
     // Top
-    int n;
+    // int n;
+    // cin >> n;
+    // for (int i=0; i<n; i++) {
+         
+    //     // spaces
+    //     for (int j=0; j<n-i-1; j++) {
+    //         cout << " ";
+    //     }
+    //     cout << "*";
+
+    //     if (i !=0) {
+    //         // spaces 
+    //         for (int j = 0; j< 2*i-1; j++){
+    //             cout << " ";
+    //         }
+
+    //         cout << "*";
+    //     }
+        
+    //     cout << endl;
+
+    // }
+    // // Bottom 
+    // for (int i=0; i<n-1; i++) {
+    //     // spaces
+    //     for (int j=0; j<i+1; j++){
+    //         cout << " ";
+    //     }
+    //     cout << "*";
+
+    //     if (i != n-2) {
+    //         // spaces 
+    //         for (int j = 0; j< 2*(n-i)-5; j++){
+    //             cout << " ";
+    //         }
+    //         cout << "*";
+    //     }
+        
+    //     cout << endl;
+    // }
+
+// Butterlfly pattern
+
+    int n ;
     cin >> n;
     for (int i=0; i<n; i++) {
-         
-        // spaces
-        for (int j=0; j<n-i-1; j++) {
-            cout << " ";
+        // stars1.1
+        for (int j=0; j<i+1; j++) {
+            cout << "* ";
         }
-        cout << "*";
-
-        if (i !=0) {
-            // spaces 
-            for (int j = 0; j< 2*i-1; j++){
-                cout << " ";
-            }
-
-            cout << "*";
+        // spaces 
+        for (int j=0; j< 2*(n-i-1); j++) {
+            cout << "  ";
+        } 
+        // stars1.2
+        for (int j=0; j<i+1; j++) {
+            cout << "* ";
         }
-        
         cout << endl;
-
     }
-    // Bottom 
-    for (int i=0; i<n-1; i++) {
-        // spaces
-        for (int j=0; j<i+1; j++){
-            cout << " ";
-        }
-        cout << "*";
 
-        if (i != n-2) {
-            // spaces 
-            for (int j = 0; j< 2*(n-i)-5; j++){
-                cout << " ";
-            }
-            cout << "*";
+    for (int i=0; i<n-1; i++) {
+        // stars2.1 
+        for (int j=0; j<(n-1)-i; j++) {
+            cout << "* ";
+        } 
+        // spaces
+        for (int j=0; j<2*(i+1); j++){
+            cout << "  ";
         }
-        
+        // stars2.2
+        for (int j=0; j<(n-1)-i; j++) {
+            cout << "* ";
+        }
+
         cout << endl;
     }
 
