7dfebb38f9dd9e817dca97f1d4a277e17726c46a
Suresh <132415690+kammariSuresh@users.noreply.github.com>
2026-08-26 18:10:25 +0530

    Commit30
    Removed spaces from star output in the pattern generation.

--- chapter4/code.cpp
+++ chapter4/code.cpp
@@ -191,30 +191,30 @@ int main() {
     for (int i=0; i<n; i++) {
         // stars1.1
         for (int j=0; j<i+1; j++) {
-            cout << "* ";
+            cout << "*";
         }
         // spaces 
         for (int j=0; j< 2*(n-i-1); j++) {
-            cout << "  ";
+            cout << " ";
         } 
         // stars1.2
         for (int j=0; j<i+1; j++) {
-            cout << "* ";
+            cout << "*";
         }
         cout << endl;
     }
     for (int i=0; i<n-1; i++) {
         // stars2.1 
         for (int j=0; j<(n-1)-i; j++) {
-            cout << "* ";
+            cout << "*";
         } 
         // spaces
         for (int j=0; j<2*(i+1); j++){
-            cout << "  ";
+            cout << " ";
         }
         // stars2.2
         for (int j=0; j<(n-1)-i; j++) {
-            cout << "* ";
+            cout << "*";
         }
         cout << endl;
     }
